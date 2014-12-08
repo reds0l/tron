4680bb34cf4d33153982d8f9f0c9532123e2e3f7
Charles Shinaver <bshinaver@gmail.com>
2014-12-08 15:52:49 -0500

    Added boundary box

--- tronGame.c
+++ tronGame.c
@@ -26,6 +26,24 @@ void plotBike(lightCycle *cycle);
 
 int main()
 {
+    int offset = 50;
+    int xsize = 500;
+    int ysize = 500;
+    int xBoxSize = xsize - offset;
+    int yBoxSize = ysize - offset;
+
+
+    gfx_open(xsize, ysize, "Tron");
+
+    // Plot boundaries
+    gfx_line(offset, offset, xBoxSize, offset);
+    gfx_line(offset, offset, offset, yBoxSize);
+    gfx_line(offset, yBoxSize, xBoxSize, yBoxSize);
+    gfx_line(xBoxSize, offset, xBoxSize, yBoxSize);
+
+
+
+
     // moveBike
 
 
