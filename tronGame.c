3b1eeb4d6267fdc1e1324c114fb501f7e5909b59
Charles Shinaver <bshinaver@gmail.com>
2014-12-10 23:17:00 -0500

    Added second bike

--- tronGame.c
+++ tronGame.c
@@ -11,7 +11,8 @@ int main()
     double dt = .1;
     Color black = {0, 0, 0};
     Color white = {255, 255, 255};
-    Color bike1Color = {125, 125, 125};
+    Color bike1Color = {255, 255, 0};
+    Color bike2Color = {0, 102, 204};
     // Box Dimensions
     int offset = 50;
     int xsize = 500;
@@ -29,6 +30,15 @@ int main()
     cycle1->radius = 2;
     cycle1->color = bike1Color;
 
+    // Create cycle2
+    LightCycle *cycle2 = malloc(sizeof(LightCycle));
+    cycle2->x = 225;
+    cycle2->y = 220;
+    cycle2->orientation = right;
+    cycle2->isAlive = 1;
+    cycle2->radius = 2;
+    cycle2->color = bike2Color;
+
 
     gfx_open(xsize, ysize, "Tron");
 
@@ -38,7 +48,7 @@ int main()
 
     while(1)
     {
-        if (checkForKeyboardInput(cycle1, cycle1) == 'q')
+        if (checkForKeyboardInput(cycle1, cycle2) == 'q')
         {
             return 0;
         }
@@ -46,6 +56,8 @@ int main()
         // moveBike
         moveBike(cycle1, dt, board);
         plotBike(cycle1, cycle1->color);
+        moveBike(cycle2, dt, board);
+        plotBike(cycle2, cycle2->color);
 
         usleep(500);
         
